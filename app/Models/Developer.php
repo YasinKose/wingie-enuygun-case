aaeb65870dc612d0106e6ce079c0cc4c27f8cd84
YasinKose <yasin.kose.123@gmail.com>
2023-01-26 17:41:18 +0300

    feat: Create intensity_week accessor in Developer Model

--- app/Models/Developer.php
+++ app/Models/Developer.php
@@ -31,10 +31,51 @@ class Developer extends Model
     ];
 
     /**
+     * @var string[]
+     */
+    protected $appends = [
+        'intensity_week'
+    ];
+
+    /**
      * @return HasMany
      */
     public function tasks(): HasMany
     {
         return $this->hasMany(Task::class);
+    }
+
+    /**
+     * @return string
+     */
+    public function getIntensityWeekAttribute(): string
+    {
+        $totalHours = $this->getAttribute("intensity");
+        $hoursPerWeek = 45;
+        $hoursPerDay = $hoursPerWeek / 5;
+
+        $weeks = floor($totalHours / $hoursPerWeek);
+        $remainingHours = $totalHours % $hoursPerWeek;
+        $days = floor($remainingHours / $hoursPerDay);
+        $hours = $remainingHours % $hoursPerDay;
+
+        $output = "";
+        if ($weeks > 0) {
+            $output = "$weeks <small>weeks</small>";
+        }
+
+        if ($days > 0) {
+            $output .= " $days <small>days</small>";
+        }
+
+        if ($hours > 0) {
+            $output .= " $hours <small>hours</small>";
+        }
+
+        if (empty($output)) {
+            return "0 <small>hours</small>";
+        }
+
+        return "It is " . $output;
     }
 }
