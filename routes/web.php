f9df0c81ab4a42450d11d7152718a851123b0df1
YasinKose <yasin.kose.123@gmail.com>
2023-01-26 17:19:57 +0300

    feat: Create getDeveloperList route in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DeveloperController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'developers'], function () {
+    Route::get('/', [DeveloperController::class, "getDeveloperList"])->name("developer-list");
 });
