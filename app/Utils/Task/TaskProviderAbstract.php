cfd1b109d367738ca5fc95626caeed44a04b833b
YasinKose <yasin.kose.123@gmail.com>
2023-01-26 03:11:50 +0300

    feat: Create fetchTasks method in TaskProviderAbstract

--- app/Utils/Task/TaskProviderAbstract.php
+++ app/Utils/Task/TaskProviderAbstract.php
@@ -2,6 +2,9 @@
 
 namespace App\Utils\Task;
 
+use App\Exceptions\TaskProviderNotFoundException;
+use Illuminate\Support\Facades\Http;
+
 abstract class TaskProviderAbstract implements TaskProviderContract
 {
     /**
@@ -10,10 +13,33 @@ abstract class TaskProviderAbstract implements TaskProviderContract
     protected ?array $tasks = null;
 
     /**
-     * @return array|null
+     * @return array
      */
-    public function getTasks(): ?array
+    public function getTasks(): array
     {
+        if ($this->tasks === null) {
+            $this->fetchTasks();
+        }
+
         return $this->tasks;
+    }
+
+    /**
+     * @return void
+     */
+    private function fetchTasks(): void
+    {
+        $response = Http::get($this->getEndpoint());
+
+        if ($response->status() !== 200) {
+            throw new TaskProviderNotFoundException("Sağlayıcıdan geçerli yanıt alınamadı!");
+        }
+
+        $this->tasks = $response
+            ->collect()
+            ->map(function ($items) {
+                return $this->resolveItem($items);
+            })
+            ->toArray();
     }
 }
