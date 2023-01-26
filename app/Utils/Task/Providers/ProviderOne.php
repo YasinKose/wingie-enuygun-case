f488f78638d76da506e96bb714810b99164121b6
YasinKose <yasin.kose.123@gmail.com>
2023-01-26 03:11:01 +0300

    feat: Add resolveItem method in ProviderOne

--- app/Utils/Task/Providers/ProviderOne.php
+++ app/Utils/Task/Providers/ProviderOne.php
@@ -2,6 +2,7 @@
 
 namespace App\Utils\Task\Providers;
 
+use App\DataTransferObjects\TaskObject;
 use App\Utils\Task\TaskProviderAbstract;
 
 class ProviderOne extends TaskProviderAbstract
@@ -12,5 +13,18 @@ class ProviderOne extends TaskProviderAbstract
     public function getEndpoint(): string
     {
         return "https://www.mocky.io/v2/5d47f24c330000623fa3ebfa";
+    }
+
+    /**
+     * @param $item
+     * @return TaskObject
+     */
+    public function resolveItem($item): TaskObject
+    {
+        return TaskObject::make(
+            $item['id'],
+            $item['zorluk'],
+            $item['zorluk']
+        );
     }
 }
