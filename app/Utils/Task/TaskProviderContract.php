3617b5dc215fdacc79a5b53916de055853eb1aa8
YasinKose <yasin.kose.123@gmail.com>
2023-01-26 03:10:13 +0300

    feat: Add resolveItem method in TaskProviderContract

--- app/Utils/Task/TaskProviderContract.php
+++ app/Utils/Task/TaskProviderContract.php
@@ -2,12 +2,20 @@
 
 namespace App\Utils\Task;
 
+use App\DataTransferObjects\TaskObject;
+
 interface TaskProviderContract
 {
     /**
      * @return string
      */
     public function getEndpoint(): string;
+
+    /**
+     * @param $item
+     * @return TaskObject
+     */
+    public function resolveItem($item): TaskObject;
 
     /**
      * @return array|null
